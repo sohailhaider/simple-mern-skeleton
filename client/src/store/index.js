aa49a4faca06ebaab0e5034c4801f4661e2b9f1d
Sohail Haider <sohail.haider@synavos.com>
2021-08-19 23:53:04 +0500

    persisten state added

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "./slices/loginSlice";
 
-export default configureStore({
+const STATE_NAME = "reduxState";
+const persistedState = localStorage.getItem(STATE_NAME)
+  ? JSON.parse(localStorage.getItem(STATE_NAME))
+  : {};
+const store = configureStore({
   reducer: {
     login: loginReducer,
   },
+  preloadedState: persistedState,
 });
+store.subscribe(() => {
+  localStorage.setItem(STATE_NAME, JSON.stringify(store.getState()));
+});
+export default store;

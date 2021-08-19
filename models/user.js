d30e9dccd8c07db95614c65b25c235739c39b52f
Sohail Haider <sohail.haider@synavos.com>
2021-08-19 18:47:15 +0500

    indentations switched

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const options = {
-    toObject: {virtuals: true},
-    toJSON: {virtuals: true}
+  toObject: { virtuals: true },
+  toJSON: { virtuals: true },
 };
 
 const schema = new mongoose.Schema(
-    {
-        fullname: { type: String, require: false },
-        email: { type: String, require: true },
-        password: { type: String, require: true }
-    },
-    options
+  {
+    fullname: { type: String, require: false },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+  },
+  options
 );
 
 const User = mongoose.model("User", schema);
